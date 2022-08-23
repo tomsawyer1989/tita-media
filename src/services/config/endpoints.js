e8f5aa99ac3b099178795576da719c2aef8ca9a2
Jorge Leonardo Herrera Ortiz <jorge_skidrow@hotmail.com>
2022-08-23 17:41:14 -0500

    fix(config) serialize function deleted

--- src/services/config/endpoints.js
+++ src/services/config/endpoints.js
@@ -1,16 +1,5 @@
 const baseUrl = 'https://dummyapi.io/data/v1/';
 
-function serialize(obj) {
-    const serialzedParams = Object.keys(obj)
-        .reduce((accumulator, currentValue) => {
-            accumulator.push(`${ currentValue }=${ obj[currentValue] }`);
-            return accumulator;
-        }, [])
-        .join('&');
-  
-    return `?${ serialzedParams }`;
-}
-
 export const getPosts = () => `${ baseUrl }post`;
 export const getCommentsByPost = (id) => `${ baseUrl }post/${ id }/comment`;
 export const getPostUser = (id) => `${ baseUrl }user/${ id }`;
